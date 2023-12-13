9fe392a4f1d158a2a4e1409c2f3a3ab296c5b70f
VinayAsangi <vinayasangi9051@gmail.com>
2023-12-13 13:44:02 +0530

    comit success

---
+++ src/main/java/com/jsp/Lenskart/App.java
@@ -0,0 +1,13 @@
+package com.jsp.Lenskart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
